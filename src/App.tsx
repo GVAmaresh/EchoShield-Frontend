67d84df884da09f2f0f351675dc77126d93fa4f4
GVAmaresh <reshama0302@gmail.com>
2024-11-12 19:43:46 +0530

    frontend part finished

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,8 @@ import NavBar from './components/NavBar/NavBar';
 import MainBody from './components/MainBody/MainBody';
 
 export default function App() {
-  const a = "agg"
     return <div className="">
-      {/* <NavBar/>
-      <MainBody/> */}
-      <div className=" flex justify-center font-extrabold">
-        {a}
-      </div>
+      <NavBar/>
+      <MainBody/>
     </div>;
 }
